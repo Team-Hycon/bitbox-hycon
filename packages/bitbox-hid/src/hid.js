4faae04fd19869a7c453068196059040d6d7bd08
Dulguun <dulguun@glosfer.com>
2018-08-08 15:54:42 +0900

    style: use 4 spaces instead of 2

--- packages/bitbox-hid/src/hid.js
+++ packages/bitbox-hid/src/hid.js
@@ -3,27 +3,28 @@
 const HID = require('node-hid')
 
 /**
- * Returns the device info of the digital bit box or null if no 
+ * Returns the device info of the digital bit box or null if no
  * bit box was found.
  */
 exports.getDeviceInfo = function() {
-  const devices = HID.devices()
-  var deviceInfo = devices.find( function(d) {
-    var isBitBox = d.vendorId === 0x03eb && d.productId === 0x2402;
-    // usage page is correctly set on Windows/Mac, 
-    // interface is correctly set on Linux.
-    return isBitBox && (d.usagePage === 0xFFFF || d.interface === 0);
-  });
-  if (deviceInfo) {
-    return deviceInfo;
-  }
-  return null;
+    const devices = HID.devices()
+    var deviceInfo = devices.find( function(d) {
+        var isBitBox = d.vendorId === 0x03eb && d.productId === 0x2402;
+        // usage page is correctly set on Windows/Mac,
+        // interface is correctly set on Linux.
+        return isBitBox && (d.usagePage === 0xFFFF || d.interface === 0);
+    });
+
+    if (deviceInfo) {
+        return deviceInfo;
+    }
+    return null;
 }
 
 /**
  * Opens the device.
  */
 exports.openDevice = function(devicePath) {
-  return new HID.HID(devicePath);
+    return new HID.HID(devicePath);
 }
 
